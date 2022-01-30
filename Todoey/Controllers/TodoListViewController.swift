e5946e07d5f73516dcf3e216302587d88a82497c
abdoomaaz <abodeh98@gmail.com>
2022-01-30 17:37:38 +0100

    Adds search bar and the ability to search for a todey item

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -6,19 +6,19 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
-
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
-        itemArray.append(Item(title: "test", done: true))
         super.viewDidLoad()
         loadItems()
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -41,6 +41,10 @@ extension TodoListViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        //      Delete items onSelect and YES ORDER MATTERS
+        //      context.delete(itemArray[indexPath.row])
+        //      itemArray.remove(at: indexPath.row)
         saveItems()
     }
     
@@ -53,13 +57,17 @@ extension TodoListViewController {
         let action  = UIAlertAction(title: "Add new Item", style: .default) { action in
             
             if let safeText = textfield.text {
-                if !safeText.isEmpty{
-                    self.itemArray.append(Item (title:safeText,done: false))
+                if !safeText.isEmpty {
+                    
+                    let newItem = Item(context: self.context)
+                    newItem.title = safeText
+                    newItem.done = false
+                    self.itemArray.append(newItem)
                     self.saveItems()
                 }
             }
         }
-         
+        
         alert.addTextField { alertTextField in
             alertTextField.placeholder  = "Create new Todoey"
             textfield = alertTextField
@@ -74,28 +82,61 @@ extension TodoListViewController {
 
 // MARK: - Fetch & Save Items
 extension TodoListViewController {
-    
-    func saveItems(){
-        let encoder = PropertyListEncoder()
+    //        Commented out is using Codable .plist
+    func saveItems() {
+        //        let encoder = PropertyListEncoder()
         
         do {
-            let data = try encoder.encode(self.itemArray)
-            try data.write(to: self.dataFilePath!)
+            //            let data = try encoder.encode(self.itemArray)
+            //            try data.write(to: self.dataFilePath!)
+            try self.context.save()
         }
-        catch{
-            print("Error encoding item array: \(error)")
+        catch {
+            //          print("Error encoding item array: \(error)")
+            print("Error Saving item array: \(error)")
         }
         tableView.reloadData()
     }
     
-    func loadItems(){
-        if  let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-                itemArray = try decoder.decode([Item].self, from: data)
-            }
-            catch{
-                print("Error when decoding item Array: \(error)")
+    func loadItems(with req: NSFetchRequest<Item> = Item.fetchRequest()){
+        //        if  let data = try? Data(contentsOf: dataFilePath!) {
+        //            let decoder = PropertyListDecoder()
+        
+        //        let req : NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+            itemArray = try context.fetch(req)
+            //              itemArray = try decoder.decode([Item].self, from: data)
+        }
+        catch{
+            //                print("Error when decoding item Array: \(error)")
+            print("Error when Fetching Items: \(error)")
+        }
+        //        }
+        tableView.reloadData()
+    }
+
+}
+
+
+// MARK: - SearchBarDelegare
+extension TodoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        DispatchQueue.main.async {
+            searchBar.resignFirstResponder()
+        }
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+        }
+        else {
+            if let searchText = searchBar.text {
+                let request: NSFetchRequest<Item> = Item.fetchRequest()
+                request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchText)
+                request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+                loadItems(with: request)
             }
         }
     }
